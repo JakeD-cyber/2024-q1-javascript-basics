01b9dc4c178e60379ac48af9ca2a5bd560c15e7c
Jake Dudley <jake2007andrew@gmail.com>
2024-09-23 17:29:38 -0600

    new

--- scripts/rps-game.js
+++ scripts/rps-game.js
@@ -1,6 +1,23 @@
 //@ts-check
 
-let playerControls = document.getElementById("player-controls") 
+let playerControls = document.getElementById("player-controls"); 
+
+//game-results
+/** @type { HTMLElement } */
+//@ts-ignore We know player controls is not null
+let gameResultHeader = document.getElementById("game-results");
+
+/** @type { HTMLElement } */
+//@ts-ignore We know game result header is not null
+let gameRestartSection = document.getElementById("game-results");
+
+/** @type { HTMLElement } */
+//@ts-ignore We know game restart section is not null
+let gameRestartSection = document.getElementById("game-results");
+
+gameResultHeader?.innerText = "Hey there!!";
+
+let isGameOver = false;
 
 let weapons = [
     {
@@ -17,22 +34,68 @@ let weapons = [
     }
 ];
 
-function pickRandomWeapon(weapons) {
-    let randy = Math.floor(Math.random() * weapons.length);
-    return weapons[randy];
-} 
+function determineOutcome(playerWeapon, computerWeapon) {
+    if (playerWeapon.type === computerWeapon.type) {
+        return "it's a tie!";
+    }
+
+    isGameOver = true;
+    showRestart();
+
+    if (playerWeapon.beats === computerWeapon.type) {
+        return 'Player wins! ${playerWeapon.type} beats ${computerWeapon.type}'
+    }
+
+    return 'Computer wins! ${computerWeapon.type} beats ${playerWeapon.type}';
+
+}
 
 function playerControlHandler(e) {
- if (playerWeapon.type === computer.Weapon.type) {
-     return "its a tie!";
- }
+    if(e.target.id === "btn-restart")
+        isGameOver = false;
+    showPlayerControls();
+    gameResultHeader.InnerText ="";
+
 }
 
 
 
-function playerControlHandler(e) {
+
+  if(isGameOver) {
+      return;
+    }
     let weaponName = e.target.innerText;
-    console.log(weaponName, "was selected");
+    let playerWeapon = weapons.find(w => w.type === weaponName);
+
+    if(!playerWeapon) {
+        console.log("ERROR! player weapon undefined");
+        return;
+    }
+
+    letcomputerWeapon = PickRandomWeapon
+
+    let result = determineOutcome(playerWeapon, computerWeapon);
+    gameResultHeader?.innerText = result;
+    console.log(result);
+
+  function gameRestartHandler(e) {
+}
+
+functionShowPlayerControls() {
+    gameRestartSection.style.display = "initial";
+    playerControls.style.display = "none";
+
+function showRestart() }
+    functionShowPlayerControls() {
+    gameRestartSection.style.display = "none";
+    playerControls.style.display = "initial";
+   
+    }
+
+functionShowPlayerControls() {
+
 }
 
 playerControls?.addEventListener("click", playerControlHandler);
+
+gameRestartSection.addEventListener("click", gameRestartHandler);
