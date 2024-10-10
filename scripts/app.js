a8e1aab7a3a09bd6f9e5d9dd2aedfe297618bebd
Jake Dudley <jake2007andrew@gmail.com>
2024-10-10 16:10:33 -0600

    10

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,59 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
 class SquareShape {
+      constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.width = 50;
+        this.height = this.width;
+        this.hue = 0;
+
+        this.speedMult = 11;
+        this.speedX = Math.floor(Math.random() * this.speedMult) + 1;
+        this.speedY = Math.floor(Math.random() * this.speedMult) + 1;
+        this.dirX = 1;
+        this.dirY = 2;
+
+    }
+
+    update() {
+        this.x += this.speedX * this.dirX;
+        this.y += this.speedY * this.dirY;
+        this.hue++;
+
+        if(this.hue > 360) {
+            this.hue = 0;
+
+        }
+
+        if(this.y < 0) {
+            // offscreen top so move down
+            this.dirY = 1;
+       } else if(this.y + this.height> canvas.height) {
+           // offscreen bottom so move up
+           this.dirY = -1;
+       }
+   
+
+    }
+
+    draw() {
+        
+screensize = (800,600)
+screen = display.set_mode(screensize,0)
+display.set_caption("Animation Test")
+
+
+
+x = 100
+y = 200
+dx = 2
+dy = 2
+go = True
+
+    Colour = RED
+    }
 }
 
 let s1 = new SquareShape(0, 0);
@@ -20,71 +73,13 @@ for (let i = 0; i <100; i++) {
 }
 
 let LastTime = 0;
-
-function drawLoop(timestamp); {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    let elaspedTime = timestamp - LastTime;
-    lastTime = timestamp;
-
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-
-        this.width = 50;
-        this.height = this.width;
-        this.hue = 0;
-
-        this.speedMult = 11;
-        this.speedX = Math.floor(Math.random() * this.speedMult) + 1;
-        this.speedX = Math.floor(Math.random() * this.speedMult) + 1;
-        this.dirX = 1;
-        this.dirY = 2;
-
-    }
-
-    update() {
-        this.x += this.speedX; * this.dirX;
-        this.y += this.speedY; * this.dirY;
-        this.hue++;
-
-        if(this.hue > 360) {
-            this.hue = 0;
-    }
-
-    if(this.y < 0) {
-         // offscreen top so move down
-         this.dirY = 1;
-    } else if(this.y + this.height> canvas.height) {
-        // offscreen bottom so move up
-        this.dirY = -1;
-    }
-
-}
-
-draw() {}
-
-ctx.fillStyle= "hsla(0, 100%, 50%, 100%)";
-//"#ff0000";
-//"red";
-
-ctx.fillRect(0, 0, 50, 50);
-
-ctx.beginPath();
-ctx.arc(100, 100, 25, 0, Math.PI * 2);
-ctx.fill();
-
-let LastTime = 0;
 let hue = 0;
 
-let x1 = 0;
-let y1 = 0;
-let speedX = 1;
-let speedY = 0.5;
 
-function drawLoop(timestamp); {
+function drawLoop(timestamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let elaspedTime = timestamp - LastTime;
-    lastTime = timestamp;
+    LastTime = timestamp;
 
     s1.draw();
     s1.update();
@@ -96,20 +91,9 @@ function drawLoop(timestamp); {
     }
 
 
-    ctx.fillStyle = `hsla(${hue}, 100%, 50%, 100%)`;
-    ctx.fillRect(x1, y1, 100, 100);
-
-    // hue = hue + 1;
-   // hue += 1;
-   hue++; // add one to the current value of hue
-
-   x1 += speedX;
-   y1 += speedY;
-
-
    // console.log(elapsedTime);
 
-    
+   window.requestAnimationFrame(drawLoop);    
 }
 
 window.requestAnimationFrame(drawLoop);
